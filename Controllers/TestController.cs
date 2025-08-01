fd1265d6119df8982a082760f885b8375a600967
Kuro-D-Shiro <iljashirobocov@gmail.com>
2025-08-01 17:41:36 +0400

    Add controller for tests and learning

---
+++ Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+using ZeonParser.Parser.Interfaces;
+using ZeonParser.Parser.Settings;
+using AngleSharp.Html.Parser;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+
+namespace ZeonParser.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class TestController(IHtmlLoader okak, IOptions<ZeonParserSettings> option) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> Test()
+        {
+            var HTMLString = await okak.LoadPageByURL(option.Value.Url);
+
+            HtmlParser htmlParser = new();
+            IHtmlDocument doc = htmlParser.ParseDocument(HTMLString);
+            List<IElement> elements = doc.QuerySelectorAll(".catalog-menu-list-one").ToList();
+
+            var aboba = elements[7].TextContent;
+
+            return Ok();
+        }
+    }
+}
