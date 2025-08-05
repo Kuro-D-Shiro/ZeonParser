d4dd0cc001522c858cf186a9435f52cd0f348660
Kuro-D-Shiro <iljashirobocov@gmail.com>
2025-08-05 12:35:11 +0400

    Update models

--- Models/Category.cs
+++ Models/Category.cs
@@ -5,6 +5,9 @@
         public int CategoryId { get; set; } 
         public string Name { get; set; }
         public string Link { get; set; }
+        public long? ParentCategoryId { get; set; }
         public Category? ParentCategory { get; set; }
+        public ICollection<Category>? ChildCategories { get; set; } = null;
+        public ICollection<Product> Products { get; set; } = [];
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -2,12 +2,13 @@
 {
     public class Product
     {
-        public string Code { get; set; } 
+        public long ProductId { get; set; } 
         public string Name { get; set; } 
         public Image Image { get; set; }
-        public Decimal? PriceWithoutDiscount { get; set; }
-        public Decimal PriceWithDiscount { get; set; }
+        public Decimal? OldPrice { get; set; }
+        public Decimal CurrentPrice { get; set; }
         public bool InStock { get; set; }
+        public long CategoryId { get; set; }
         public Category Category { get; set; } = new();
     }
     public class Image
