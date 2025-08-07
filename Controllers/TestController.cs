58b97be02c5d11b87515d08b55c9303c3c360e24
Kuro-D-Shiro <iljashirobocov@gmail.com>
2025-08-07 15:50:03 +0400

    Fix DFS algorithm for parsing

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -17,17 +17,7 @@ namespace ZeonService.Controllers
         [HttpGet]
         public async Task<ActionResult> Test()
         {
-            var mainCategoryElement = await ZeonPage.TryCreate(await okak.LoadPageByURL("https://zeon18.ru/zeon/"));
-            var els = mainCategoryElement.GetElementsBySelector(".catalog-index-cell");
-            
-            await zeonParser.ParseDFS();
-            /*var page = await ZeonPage.TryCreate(HTMLString);
-            if (page == null)
-            {
-                return StatusCode(500);
-            }
-            await Parse(page, ".catalog-menu-list-one");*/
-
+            await zeonParser.Parse();
             return Ok();
         }
 
@@ -48,104 +38,10 @@ namespace ZeonService.Controllers
     public interface IZeonParser
     {
         Task Parse();
-        Task ParseDFS();
     }
 
-    public class ZeonParser(IHtmlLoader htmlLoader, IOptions<ZeonParserSettings> options) : IZeonParser
-    {
-        private readonly IHtmlLoader _htmlLoader = htmlLoader;
-        private readonly ZeonParserSettings _parserSettings = options.Value;
-        private readonly string _mainCategoriesSelector = ".catalog-menu-list-one";
-
-        /*public async Task<T> RecursiveParse<T>(string selector)
-        {
-
-        }*/
-
-        public async Task ParseDFS()
-        {
-            var mainPage = await ZeonPage.TryCreate(await _htmlLoader.LoadPageByURL(_parserSettings.Url));
-
-            var mainCategoryElements = mainPage?.GetElementsBySelector(_mainCategoriesSelector).Skip(5)
-                ?? throw new Exception("Не удалось создать страницу по HTML."); //nullable ???
-
-            foreach (var mainCategoryElement in mainCategoryElements)
-            {
-                var categoryParser = new ZeonCategoryParser(mainCategoryElement);
-                var currentMainCategory = categoryParser.Parse(null);
-
-                var mainCategoryPage = await ZeonPage.TryCreate(currentMainCategory.Link); //nullable ???
-
-                //использовать второй стек для категорий, чтоб из привязывать к дочерним параллельно парсингу !!!
-                Stack<ZeonPage> zeonPages = new Stack<ZeonPage>();
-                var el = await ZeonPage.TryCreate(await _htmlLoader.LoadPageByURL(currentMainCategory.Link)); //тоже переделать
-                zeonPages.Push(el/*mainCategoryPage*/); //nullable ???
-                Stack<Category> categories = new Stack<Category>();
-                categories.Push(currentMainCategory);
-                Category currentSubcategory;
-
-                while (zeonPages.Count > 0)
-                {
-                    var page = zeonPages.Pop();
-                    var parentCategory = categories.Pop();
-
-                    var subcategoryIndexCells = page.GetElementsBySelector(".catalog-index-cell");
-
-                    if(!subcategoryIndexCells.Any())
-                    {
-                        var els = page.GetElementsBySelector(".catalog-grid-cell");
-                    }
-
-                    foreach (var subcategoryIndexCell in subcategoryIndexCells)
-                    {
-                        categoryParser = new ZeonCategoryParser(subcategoryIndexCell);
-                        currentSubcategory = categoryParser.Parse(parentCategory);
-                        categories.Push(currentSubcategory);
-
-                        el = await ZeonPage.TryCreate(parentCategory.Link); //переделать ес чо
-                        zeonPages.Push(el); //nullable ???
-                    }
-                }
-            }    
-        }
-
-        public async Task Parse()
-        {
-            var mainPage = await ZeonPage.TryCreate(await _htmlLoader.LoadPageByURL(_parserSettings.Url));
-
-            var mainCategoryElements = mainPage?.GetElementsBySelector(_mainCategoriesSelector)
-                ?? throw new Exception("Не удалось создать страницу по HTML.");
-            /*
-                            var tasksForCreateZeonPages = mainCategoryElements
-                                .Select(x => ZeonPage.TryCreate(x.QuerySelector("a")?.GetAttribute("href")))
-                                .ToList();
-                            List<ZeonPage> mainCategoryPages = (await Task.WhenAll(tasksForCreateZeonPages))
-                                .Where(p => p != null) 
-                                .ToList();*/
-
-            foreach (var mainCategoryElement in mainCategoryElements)
-            {
-                var categoryParser = new ZeonCategoryParser(mainCategoryElement);
-                var currentMainCategory = categoryParser.Parse(null);
-
-                var mainCategoryPage = await ZeonPage.TryCreate(currentMainCategory.Link);
-
-                var subcategoryIndexCells = mainCategoryPage.GetElementsBySelector(".catalog-index-cell");
-                foreach (var subcategoryIndexCell in subcategoryIndexCells)
-                {
-                    while (subcategoryIndexCells.Any())
-                    {
-                        Category currentSubcategory;
-                        /*foreach (var subcategoryIndexCell in subcategoryIndexCells)
-                        {
-                            categoryParser = new ZeonCategoryParser(subcategoryIndexCell);
-                            currentSubcategory = categoryParser.Parse(currentMainCategory);
-                        }*/
-                    }
-                }
-            }
-        }
-    }
+    
+}
 
     class ZeonCategoryParser(IElement categoryElement)
     {
@@ -175,30 +71,11 @@ namespace ZeonService.Controllers
             return _htmlDocument.QuerySelectorAll(selector);
         }
 
-        public static async Task<ZeonPage?> TryCreate(string pageSource)
+        public static async Task<ZeonPage> TryCreate(string pageSource)
         {
             HtmlParser htmlParser = new();
             var doc = await htmlParser.ParseDocumentAsync(pageSource);
 
-            if (doc is null)
-                return null;
-
             return new ZeonPage(doc);
         }
     }
-}
-
-/*HtmlParser htmlParser = new();
-            IHtmlDocument doc = htmlParser.ParseDocument(HTMLString);
-            List<IElement> elements = doc.QuerySelectorAll(".catalog-menu-list-one").ToList();
-            var aboba = elements[7].QuerySelector("a")?.GetAttribute("href");
-
-            HTMLString = await okak.LoadPageByURL(aboba);
-            doc = htmlParser.ParseDocument(HTMLString);
-            elements = doc.QuerySelectorAll(".catalog-index-cell").ToList();
-            aboba = elements[7].QuerySelector(".catalog-index-catalog-title")?.GetAttribute("href");
-
-            HTMLString = await okak.LoadPageByURL(aboba);
-            doc = htmlParser.ParseDocument(HTMLString);
-            elements = doc.QuerySelectorAll(".catalog-index-cell").ToList();
-            aboba = elements[7].QuerySelector(".catalog-index-catalog-title")?.GetAttribute("href");*/
